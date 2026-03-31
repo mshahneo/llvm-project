37521b0bb63170ff97254a54a8131652ef0f507c
Garra1980 <igor.zamyatin@intel.com>
2026-03-31 18:36:10 +0200

    Add SPIRV_ExecutionModeAttributesAttr

--- mlir/lib/Dialect/SPIRV/IR/SPIRVDialect.cpp
+++ mlir/lib/Dialect/SPIRV/IR/SPIRVDialect.cpp
@@ -1043,6 +1043,10 @@ LogicalResult SPIRVDialect::verifyOperationAttribute(Operation *op,
   } else if (symbol == spirv::getTargetEnvAttrName()) {
     if (!isa<spirv::TargetEnvAttr>(attr))
       return op->emitError("'") << symbol << "' must be a spirv::TargetEnvAttr";
+  } else if (symbol == spirv::getExecutionModeFuncAttrName()) {
+    if (!isa<spirv::ExecutionModeFuncAttributeAttr>(attr))
+      return op->emitError("'")
+             << symbol << "' must be a spirv::ExecutionModeFuncAttributeAttr";
   } else if (symbol == spirv::getLoopControlAttrName()) {
     if (!isa<spirv::LoopControlAttr>(attr))
       return op->emitError("'")
